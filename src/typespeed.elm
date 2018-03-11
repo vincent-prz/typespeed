bdf5535e5732fbf04fd36e358c36253839737f0a
Vincent Perez <vperez@yolaw.fr>
2018-03-11 18:06:31 +0100

    Add words periodically + keep focus on Input

--- src/typespeed.elm
+++ src/typespeed.elm
@@ -1,11 +1,13 @@
+import Dom
 import Html exposing (Html, br, button, div, input, span, text)
-import Html.Attributes exposing (style, type_)
-import Html.Events exposing (keyCode, on, onClick, onInput)
+import Html.Attributes exposing (autofocus, id, style, type_)
+import Html.Events exposing (keyCode, on, onBlur, onClick, onInput)
 import Json.Decode as Json
+import Task
 import Time exposing (Time, second)
 
 
-nbStepsBeforeGameOver = 12
+nbStepsBeforeGameOver = 9
 
 main =
   Html.program
@@ -20,7 +22,6 @@ main =
 
 type alias Word = String
 type alias Position = Int
-type alias Duration = Int
 type alias PositionedWord = {
   word: Word,
   pos: Position
@@ -35,7 +36,7 @@ onKeyDown tagger =
 -- MODEL
 
 type alias Model =
-  { duration : Duration,
+  { nbTicks : Int,
     wordPosList : List PositionedWord,
     currentEntry : String
   }
@@ -45,7 +46,7 @@ init : (Model, Cmd Msg)
 init =
   let
     initModel = 
-      { duration = 0,
+      { nbTicks = 0,
         wordPosList = [
           { word = "Hello", pos = 0 },
           { word = "Bye", pos = 1}
@@ -64,6 +65,8 @@ shiftWordPos { word, pos } = { word = word, pos = pos + 1 }
 displayWordPos : PositionedWord -> Html msg
 displayWordPos {word, pos} = span [ style [("padding", toString(pos * 10) ++ "px") ] ] [ text word ]
 
+addWord : List PositionedWord -> Word -> List PositionedWord
+addWord list w = list ++ [{ word = w, pos = 0 }]
 
 checkWord : List PositionedWord -> Word -> (List PositionedWord, Bool)
 checkWord lpw w =
@@ -84,6 +87,10 @@ type Msg
   = Tick
   | ChangeEntry String
   | KeyDown Int
+  -- those two actions are for keeping the focus on the input
+  -- see https://stackoverflow.com/questions/31901397/how-to-set-focus-on-an-element-in-elm
+  | FocusOnInput
+  | FocusResult (Result Dom.Error ())
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -91,11 +98,13 @@ update msg model =
   case msg of
     Tick ->
       let
+        tempList = List.map shiftWordPos model.wordPosList
+        newWordPosList = if (model.nbTicks % 3) == 0 then addWord tempList "fuck" else tempList
         newModel =
           {
             model |
-            duration = model.duration + 1,
-            wordPosList = List.map shiftWordPos model.wordPosList
+            nbTicks = model.nbTicks + 1,
+            wordPosList = newWordPosList
           }
       in
         (newModel, Cmd.none)
@@ -109,6 +118,18 @@ update msg model =
           ({ model | wordPosList = newWordPosList }, Cmd.none)
       else
         (model, Cmd.none)
+    FocusOnInput ->
+      ( model, Dom.focus "input" |> Task.attempt FocusResult )
+    FocusResult result ->
+        -- handle success or failure here
+        case result of
+            Err (Dom.NotFound id) ->
+              -- unable to find dom 'id'
+              (model, Cmd.none)
+            Ok () ->
+              -- successfully focus the dom
+              (model, Cmd.none)
+
 
 -- SUBSCRIPTIONS
 
@@ -132,5 +153,5 @@ view model =
       div []
         ((List.intersperse (br [] []) wordDisplayList) ++
         [ br [] []
-        , input [ type_ "text", onInput ChangeEntry, onKeyDown KeyDown ] []
+        , input [ id "input", type_ "text", onInput ChangeEntry, onKeyDown KeyDown, autofocus True, onBlur FocusOnInput ] []
         ])
